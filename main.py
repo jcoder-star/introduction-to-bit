14e43686f4cefeb1c71a1643857d68d6abd1bd29
jcoder-star <jemeeladeyemi@gmail.com>
2024-09-15 17:15:42 +0100

    list of operators

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("hello")
+#ist operator is called the and opertor is re[resented by &
+#And => returns 1 if both bits are 1 else 0
+print(bin(7&4))
+
+# 2nd operator is the or operator is represented by the pipe symbol /
+# OR => returns 1 if either bits are 1 
+print(bin( 5 | 10))
+
+#3rd oprator is called the XOR operator and it is represented by the power symbol ^
+#XOR returns => 1 if both bits are different
+print( bin(7^2))
+
+#left shift operator shifts thye binary number to the left 
+#it is represented by this symbol <<
